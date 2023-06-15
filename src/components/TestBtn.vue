7cb6d86da4bb7dd2e9b627b90e130a937e896f4b
Win <691871826@qq.com>
2023-06-15 21:45:51 +0800

    w-design组件库框架初始化

---
+++ src/components/TestBtn.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="TestBtn">
+        <button>我是测试要发布的按钮组件</button>
+    </div>
+</template>
+   
+  <!-- 导出组件名 -->
+<script>
+export default {
+    name: 'TestBtn'
+}
+</script> 
+<script setup>
+
+</script>
+   
+<style scoped lang="less">
+.TestBtn {
+    button {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        cursor: pointer;
+        transition: border-color 0.25s;
+    }
+
+    button:hover {
+        border-color: #646cff;
+    }
+}
+</style>
